c392ca666a31daad212b55038ca41accab115254
Aleksey100ckiy <109811250+Aleksey100ckiy@users.noreply.github.com>
2023-06-04 13:24:10 +0300

    connecting to db

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
+const mongoose = require('mongoose');
+
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST = 'mongodb+srv://OleksiiStotskiy:e95Y%40LgF3Gnf6Hm@cluster0.av1koc0.mongodb.net/db_contacts';
+
+mongoose.set('strictQuery', true);
+  
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Server running. Use our API on port: 3000");
+    console.log("Database connection successful");
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
